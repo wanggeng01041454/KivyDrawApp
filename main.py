a95c5df825d28543637dd0cac7cc425d657766c0
wanggeng <wanggeng01041454@163.com>
2023-11-22 23:45:53 +0800

    找了些例子代码，首次提交

---
+++ main.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Color, Ellipse
+
+
+class MyPaintWidget(Widget):
+
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 1, 0)
+            d = 30.
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
+
+
+class MyPaintApp(App):
+
+    def build(self):
+        return MyPaintWidget()
+
+
+if __name__ == '__main__':
+    MyPaintApp().run()
+
+
+# 能够输出 PIL.Image.Image 类型的图片，并经网络，无损压缩编码后传输到服务端；同时从服务端接收
+# todo
+# 基于kivy的良好操作界面，进行画图基本操作；同时提供提示词输入的地方
+# 将画图结果转换为 PIL.Image.Image 类型的数据；
+# 将数据进行无损压缩， zip, 7z 等
+# 经网路发送到服务端， 使用proto buf
+# 从服务端接收压缩后的图片数据；并解压缩后展示；
+# 从服务器接收的数据可以保存在本地
+
